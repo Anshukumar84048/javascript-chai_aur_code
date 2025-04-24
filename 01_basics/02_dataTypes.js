b670b91dedb309025b902b62d1faed6631936b30
Anshukumar84048 <159518903+Anshukumar84048@users.noreply.github.com>
2025-04-24 09:43:47 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  // treat all JS code as newer version
+//alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3)
+console.log("anshu")
+
+let name = "anshu"       // string datatype
+let age = 18            //numbers datatype
+let isLoggedin = false //boolean datatype
+
+// number => 2 to power 53 (range)
+// bignint
+// string => ""   (always try to write string in double quoted not in single quoted)
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+comsole.log(typeof null)        // object datatype show krega
+console.log(typeof undefined)  // undefined datatype show krega
+
