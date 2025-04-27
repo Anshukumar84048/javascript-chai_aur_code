1fea477a88ea0c13f10d4a62c64811326949c7cc
Anshukumar84048 <159518903+Anshukumar84048@users.noreply.github.com>
2025-04-27 11:11:12 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,9 +18,38 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false 
 // "anshu" => true
+
+
+
+//      **************OPERATIONS************
+
+let value = 3
+let negValue = -value
+//  console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%4) // to find remainder
+// console.log(2**2) // 2 to the power 2
+
+
+let str1 = "anshu"
+let str2 = " kumar"
+let str3 = str1 + str2
+
+console.log(str3)
+
+// ********************************//
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
